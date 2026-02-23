c4ab52020aaeae5378a31e96149560eb825a3ab8
bcrenato <bcrenato@gmail.com>
2026-02-23 16:48:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ if (MODO_SIMULACAO) {
 
     // Atualiza a tela localmente
     atualizarInterface(simNivel, litrosSimulados);
-  }, 1000); // Atualiza a cada 3 segundos
+  }, 5000); // Atualiza a cada 3 segundos
 }
 
 // ===== ESCUTA O FIREBASE EM TEMPO REAL (MODO REAL) =====
